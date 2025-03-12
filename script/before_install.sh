819ec15ce8a03aa38037471263619c0d3adfda90
ZarelaMC <zarelamanrique87@gmail.com>
2025-03-12 13:16:33 -0500

    fixed: Corrección before_install

--- script/before_install.sh
+++ script/before_install.sh
@@ -2,9 +2,11 @@
 echo "🚀 Ejecutando before_install.sh"
 if [ -d /var/www/html ]; then
   echo "🧹 Limpiando contenido antiguo"
-  rm -rf /var/www/html/*
+  sudo rm -rf /var/www/html/*
 else
   echo "📁 Creando carpeta /var/www/html"
-  mkdir -p /var/www/html
-  sudo chown ec2-user:ec2-user /var/www/html
+  sudo mkdir -p /var/www/html
 fi
+
+# Asegurar que el directorio pertenece a ec2-user
+sudo chown -R ec2-user:ec2-user /var/www/html
