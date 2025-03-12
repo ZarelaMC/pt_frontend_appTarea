b1883b0085e97338631c3f25ae7f87f531b3a0ec
ZarelaMC <zarelamanrique87@gmail.com>
2025-03-12 11:32:14 -0500

    fixed: Corrección appspec.yml y scrpt

--- script/before_install.sh
+++ script/before_install.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo "🧹 Limpiando contenido previo..."
+echo "🚀 Ejecutando before_install.sh"
+if [ -d /var/www/html ]; then
+  echo "🧹 Limpiando contenido antiguo"
+  rm -rf /var/www/html/*
+else
+  echo "📁 Creando carpeta /var/www/html"
+  mkdir -p /var/www/html
+fi
